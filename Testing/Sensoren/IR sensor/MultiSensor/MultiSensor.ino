b244d06f4caa4ab62e6e6fa9a3837fba77d137d5
Laurens van der Drift <Lauwy222@hotmail.com>
2023-02-09 12:04:03 +0100

    First test

---
+++ Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
@@ -0,0 +1,20 @@
+uint8_t IR_pins[] = {8, 9, 10, 11};
+uint8_t IR_sensoramount = sizeof(IR_pins) / sizeof(IR_pins[0]);
+
+//Struct data localisation
+struct data{
+  uint8_t number;
+  int state;
+  char sensor_type[30];
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
