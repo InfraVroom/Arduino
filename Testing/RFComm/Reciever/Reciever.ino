1d13177a49bcadf3572333220ce5901711046d89
justin040304 <22093621@student.hhs.nl>
2023-02-09 18:16:42 +0100

    Update Reciever.ino

--- Testing/RFComm/Reciever/Reciever.ino
+++ Testing/RFComm/Reciever/Reciever.ino
@@ -1,9 +1,30 @@
+#include <SPI.h> // for SPI communication
+
+#include <nRF24L01.h>
+
+#include <RF24.h>
+
+RF24 radio(8,9); // CE, CSN
+const byte address[6] = "10000"; // the address the the module
 void setup() {
-  // put your setup code here, to run once:
+
+Serial.begin(9600);
+radio.begin();
+radio.openReadingPipe(0, address);
+radio.setPALevel(RF24_PA_MAX);
+radio.startListening();
+Serial.println("starting...");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+if (radio.available()) { // if nrf has any incoming data
+Serial.println("receiving");
+char text[32] = "";
+radio.read(&text, sizeof(text));
+Serial.println(text);
 
+delay(5);
 }
+}
+
