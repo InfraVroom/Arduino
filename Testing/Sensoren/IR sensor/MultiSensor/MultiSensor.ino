ee63caf488cf2ec390954c8ca904bf785d270880
Laurens van der Drift <Lauwy222@hotmail.com>
2023-02-09 12:10:52 +0100

    Struct

--- Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
+++ Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
@@ -1,13 +1,25 @@
-uint8_t IR_pins[] = {8, 9, 10, 11};
+uint8_t IR_pins[] = {8};
 uint8_t IR_sensoramount = sizeof(IR_pins) / sizeof(IR_pins[0]);
+uint8_t i;
 
 //Struct data localisation
 struct data{
-  uint8_t number;
+  char location[5];
   int state;
   char sensor_type[30];
+};
+
+struct data IR_sensor[] ={
+{"RF ", 0, "IR Sensor "};
 }
 
+void printvalues(void){
+  for i=0, i<IR_sensoramount; i++){
+    Serial.print(IR_sensor[i].sensor_type);
+    Serial.print(IR_sensor[i].location);
+    Serial.print(IR_sensor[i].state);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
