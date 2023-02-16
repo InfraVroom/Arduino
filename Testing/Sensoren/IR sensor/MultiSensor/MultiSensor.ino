c05c8c4bd4e4390018321627df6ca23e30bbb7ee
justin040304 <22093621@student.hhs.nl>
2023-02-16 12:59:36 +0100

    working

--- Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
+++ Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
@@ -1,8 +1,8 @@
 uint8_t IR_pins[] = {3, 4, 5, 6, 7, 8, 9, 10};
 uint8_t IR_sensoramount = sizeof(IR_pins) / sizeof(IR_pins[0]);
 
-uint8_t sonartrig_pins[] = {11};
-uint8_t sonarecho_pins[] = {12};
+const int sonartrig_pins[] = {11};
+const int sonarecho_pins[] = {12};
 uint8_t Sonar_sensoramount = sizeof(sonartrig_pins) / sizeof(sonartrig_pins[0]);
 
 //Struct data localisation
@@ -28,7 +28,7 @@ struct data IR_sensor[] ={
 };
 
 struct data Sonar_sensor[] ={
-  {"test", 0, 0, 0, 0, "", "Sonar sensor"},
+  {"test", 0, 0, 0, 0, 0, "Sonar sensor"},
  // {"test", 0, 0, 0, "", "Sonar sensor"},
   //{"test", 0, 0, 0, "", "Sonar sensor"}
 };
@@ -52,20 +52,19 @@ Serial.print("IR SENSOR: ");
 void Sonarstate(){
   Serial.print("SONAR SENSOR: ");
   for (int i = 0; i<Sonar_sensoramount; i++){
-  // Clears the trigPin
   digitalWrite(sonartrig_pins[i], LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(sonarecho_pins[i], HIGH);
+  digitalWrite(sonartrig_pins[i], HIGH);
   delayMicroseconds(10);
   digitalWrite(sonartrig_pins[i], LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   Sonar_sensor[i].duration = pulseIn(sonarecho_pins[i], HIGH);
-  // Calculating the distance
   Sonar_sensor[i].distance = Sonar_sensor[i].duration * 0.034 / 2; 
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(Sonar_sensor[i].duration);
+    Serial.print(" | ");
+    Serial.print(Sonar_sensor[i].location);
+    Serial.print(" [");
+    Serial.print(Sonar_sensor[i].angle);
+    Serial.print("] : ");
+    Serial.print(Sonar_sensor[i].distance);
   }
   Serial.println(" |");
 }
@@ -82,10 +81,7 @@ void setup() {
   for (int i = 0; i<Sonar_sensoramount; i++){
     pinMode(sonarecho_pins[i], INPUT);
     pinMode(sonartrig_pins[i], OUTPUT);
-  }
-
-
- 
+  } 
 }
 
 void loop() {
