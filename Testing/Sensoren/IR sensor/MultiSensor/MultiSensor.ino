c11c3b103769621bcf485ce118c520c4b70bff77
justin040304 <22093621@student.hhs.nl>
2023-02-16 11:38:49 +0100

    updated struct

--- Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
+++ Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
@@ -1,40 +1,87 @@
-uint8_t IR_pins[] = {7, 8, 9, 10};
+uint8_t IR_pins[] = {3, 4, 5, 6, 7, 8, 9, 10};
 uint8_t IR_sensoramount = sizeof(IR_pins) / sizeof(IR_pins[0]);
+
+uint8_t sonartrig_pins[] = {11};
+uint8_t sonarecho_pins[] = {12};
+uint8_t sonarecho_sensoramount = sizeof(sonartrig_pins) / sizeof(sonartrig_pins[0]);
+long duration;
+int distance;
 
 //Struct data localisation
 struct data{
   char location[5];
-  int state;
+  int state; //IR
+  long duration; //US
+  int distance; //US
+  int angle; //US
+  int anglepolarity; //0 = min, 1 = plus 
   char sensor_type[30];
 };
 
 struct data IR_sensor[] ={
-{"RF ", 0, "IR Sensor "},
-{"LF", 0, "IR Sensor "},
-{"LB", 0, "IR Sensor "},
-{"RB", 0, "IR Sensor "}
+{"RF", 0, 0, 0, 0, 1, "IR Sensor "},
+{"LF", 0, 0, 0, 0, 0, "IR Sensor "},
+{"LB", 0, 0, 0, 0, 1, "IR Sensor "},
+{"RB", 0, 0, 0, 0, 0, "IR Sensor "},
+{"RF", 0, 0, 0, 0, 1, "IR Sensor "},
+{"LF", 0, 0, 0, 0, 0, "IR Sensor "},
+{"LB", 0, 0, 0, 0, 1, "IR Sensor "},
+{"RB", 0, 0, 0, 0, 0, "IR Sensor "}
+};
+
+struct data sonar_sensor[] ={
+  {"test", 0, 0, 0, 0, "", "Sonar sensor"},
+ // {"test", 0, 0, 0, "", "Sonar sensor"},
+  //{"test", 0, 0, 0, "", "Sonar sensor"}
 };
 
 void IRstate() { //Reading and printing(If detected) IR state
+Serial.print("IR SENSOR: ");
   for (int i = 0; i<IR_sensoramount; i++){
     IR_sensor[i].state = digitalRead(IR_pins[i]);
-    if (IR_sensor[i].state == 0){ // Check if the pin high or not
+    Serial.print(" | ");
+    Serial.print(IR_sensor[i].location);
+    Serial.print(" [");
+    if(IR_sensor[i].anglepolarity==1){Serial.print("+");}
+    else {Serial.print("-");}
+    Serial.print(IR_sensor[i].angle);
+    Serial.print("] : ");
+    Serial.print(IR_sensor[i].state);
+  }
+  Serial.println(" |"); 
+   }
+
+void Sonarstate(){
+Serial.print("SONAR SENSOR: ");
+  for (int i = 0; i<IR_sensoramount; i++){
     IR_sensor[i].state = digitalRead(IR_pins[i]);
-    Serial.print("Detected ");
-    Serial.print(IR_sensor[i].sensor_type);
-    Serial.print(": ");
-    Serial.println(IR_sensor[i].location);
-  }}}
+    Serial.print(" | ");
+    Serial.print(IR_sensor[i].location);
+    Serial.print(" [");
+    if(IR_sensor[i].anglepolarity==1){Serial.print("+");}
+    else {Serial.print("-");}
+    Serial.print(IR_sensor[i].angle);
+    Serial.print("] : ");
+    Serial.print(IR_sensor[i].state);
+  }
+  Serial.println(" |");
+}
 
 void setup() {
   //Open Serial Monitor
-  Serial.begin(9600); 
+  Serial.begin(115200); 
   Serial.println("Serial Working"); 
   
   //Declaring IRpins
   for (int i = 0; i<IR_sensoramount; i++){
     pinMode(IR_pins[i], INPUT);
   }
+  for (int i = 0; i<sonarecho_sensoramount; i++){
+    pinMode(sonarecho_pins[i], INPUT);
+    pinMode(sonartrig_pins[i], OUTPUT);
+  }
+
+
  
 }
 
