58b7e082446458ca5833bc1c9051450d99b75636
justin040304 <22093621@student.hhs.nl>
2023-02-16 13:51:29 +0100

    Working multi sensors

--- Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
+++ Testing/Sensoren/IR sensor/MultiSensor/MultiSensor.ino
@@ -1,8 +1,8 @@
-uint8_t IR_pins[] = {3, 4, 5, 6, 7, 8, 9, 10};
+uint8_t IR_pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 uint8_t IR_sensoramount = sizeof(IR_pins) / sizeof(IR_pins[0]);
 
-const int sonartrig_pins[] = {11};
-const int sonarecho_pins[] = {12};
+const int sonartrig_pins[] = {10, 12, 14};
+const int sonarecho_pins[] = {11, 13, 15};
 uint8_t Sonar_sensoramount = sizeof(sonartrig_pins) / sizeof(sonartrig_pins[0]);
 
 //Struct data localisation
@@ -17,26 +17,66 @@ struct data{
 };
 
 struct data IR_sensor[] ={
-{"RF", 0, 0, 0, 0, 1, "IR Sensor "},
-{"LF", 0, 0, 0, 0, 0, "IR Sensor "},
-{"LB", 0, 0, 0, 0, 1, "IR Sensor "},
-{"RB", 0, 0, 0, 0, 0, "IR Sensor "},
-{"RF", 0, 0, 0, 0, 1, "IR Sensor "},
-{"LF", 0, 0, 0, 0, 0, "IR Sensor "},
-{"LB", 0, 0, 0, 0, 1, "IR Sensor "},
-{"RB", 0, 0, 0, 0, 0, "IR Sensor "}
+{"RF", 0, 0, 0, 0, 1, "IR Sensor"},
+{"LF", 0, 0, 0, 0, 0, "IR Sensor"},
+{"LB", 0, 0, 0, 0, 1, "IR Sensor"},
+{"RB", 0, 0, 0, 0, 0, "IR Sensor"},
+{"RF", 0, 0, 0, 0, 1, "IR Sensor"},
+{"LF", 0, 0, 0, 0, 0, "IR Sensor"},
+{"LB", 0, 0, 0, 0, 1, "IR Sensor"},
+{"RB", 0, 0, 0, 0, 0, "IR Sensor"}
 };
 
-struct data Sonar_sensor[] ={
-  {"test", 0, 0, 0, 0, 0, "Sonar sensor"},
- // {"test", 0, 0, 0, "", "Sonar sensor"},
-  //{"test", 0, 0, 0, "", "Sonar sensor"}
+
+struct data Sonar_sensor[] ={ //declaring 3 sonar sensors by using a struct
+  {"FRONT", 0, 0, 0, 0, 0, "Sonar sensor"},
+  {"LEFT", 0, 0, 0, 0, "Sonar sensor"},
+  {"RIGHT", 0, 0, 0, 0, "Sonar sensor"}
 };
 
-void IRstate() { //Reading and printing(If detected) IR state
-Serial.print("IR SENSOR: ");
+void IRstate() { //Reading IR state
   for (int i = 0; i<IR_sensoramount; i++){
     IR_sensor[i].state = digitalRead(IR_pins[i]);
+   }}
+
+void Sonarstate(){ //Reading Sonar state/distance
+  for (int i = 0; i<Sonar_sensoramount; i++){
+  digitalWrite(sonartrig_pins[i], LOW); 
+  delayMicroseconds(2);
+  digitalWrite(sonartrig_pins[i], HIGH); // set trigger pin to High to send a pulse into the real world
+  delayMicroseconds(10);
+  digitalWrite(sonartrig_pins[i], LOW); 
+  Sonar_sensor[i].duration = pulseIn(sonarecho_pins[i], HIGH); // measure the time it takes to receive the pulse
+  Sonar_sensor[i].distance = Sonar_sensor[i].duration * 0.034 / 2; //calculate the distance in cm
+  }}
+
+void setup() {
+  //Open Serial Monitor
+  Serial.begin(115200); 
+  Serial.println("Serial Working"); 
+  //Declaring IRpins
+  for (int i = 0; i<IR_sensoramount; i++){
+    pinMode(IR_pins[i], INPUT);
+  }
+  for (int i = 0; i<Sonar_sensoramount; i++){
+    pinMode(sonarecho_pins[i], INPUT);
+    pinMode(sonartrig_pins[i], OUTPUT);
+  }}
+
+void debug(){ //print the values from the sensors to the Serial Monitor
+  Serial.print("SONAR SENSOR:");
+  for (int i = 0; i<Sonar_sensoramount; i++){
+    Serial.print(" | ");
+    Serial.print(Sonar_sensor[i].location);
+    Serial.print(" [");
+    if(Sonar_sensor[i].anglepolarity==1){Serial.print("+");}
+    else {Serial.print("-");}
+    Serial.print(Sonar_sensor[i].angle);
+    Serial.print("] : ");
+    Serial.print(Sonar_sensor[i].distance);
+  }
+  Serial.print(" |     IR SENSOR:");
+  for (int i = 0; i<IR_sensoramount; i++){
     Serial.print(" | ");
     Serial.print(IR_sensor[i].location);
     Serial.print(" [");
@@ -49,43 +89,9 @@ Serial.print("IR SENSOR: ");
   Serial.println(" |"); 
    }
 
-void Sonarstate(){
-  Serial.print("SONAR SENSOR: ");
-  for (int i = 0; i<Sonar_sensoramount; i++){
-  digitalWrite(sonartrig_pins[i], LOW);
-  delayMicroseconds(2);
-  digitalWrite(sonartrig_pins[i], HIGH);
-  delayMicroseconds(10);
-  digitalWrite(sonartrig_pins[i], LOW);
-  Sonar_sensor[i].duration = pulseIn(sonarecho_pins[i], HIGH);
-  Sonar_sensor[i].distance = Sonar_sensor[i].duration * 0.034 / 2; 
-    Serial.print(" | ");
-    Serial.print(Sonar_sensor[i].location);
-    Serial.print(" [");
-    Serial.print(Sonar_sensor[i].angle);
-    Serial.print("] : ");
-    Serial.print(Sonar_sensor[i].distance);
-  }
-  Serial.println(" |");
-}
-
-void setup() {
-  //Open Serial Monitor
-  Serial.begin(9600); 
-  Serial.println("Serial Working"); 
-  
-  //Declaring IRpins
-  for (int i = 0; i<IR_sensoramount; i++){
-    pinMode(IR_pins[i], INPUT);
-  }
-  for (int i = 0; i<Sonar_sensoramount; i++){
-    pinMode(sonarecho_pins[i], INPUT);
-    pinMode(sonartrig_pins[i], OUTPUT);
-  } 
-}
 
 void loop() {
   IRstate();
   Sonarstate();  
-  
+  //debug(); // this function is used if you want to see the data in the Serial Monitor
 }
