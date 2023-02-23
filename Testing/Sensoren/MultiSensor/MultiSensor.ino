e2d00b7049d0c5059b39ae854a4e9108389bc250
Laurens van der Drift <Lauwy222@hotmail.com>
2023-02-23 12:36:27 +0100

    Added BNO055 support

--- Testing/Sensoren/MultiSensor/MultiSensor.ino
+++ Testing/Sensoren/MultiSensor/MultiSensor.ino
@@ -1,6 +1,23 @@
+//Library BNO055
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+//Pins
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire);
+
+//IR SENSOR
 uint8_t IR_pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 uint8_t IR_sensoramount = sizeof(IR_pins) / sizeof(IR_pins[0]);
 
+//SONAR SENSOR
 const int sonartrig_pins[] = {10, 12, 14};
 const int sonarecho_pins[] = {11, 13, 15};
 uint8_t Sonar_sensoramount = sizeof(sonartrig_pins) / sizeof(sonartrig_pins[0]);
@@ -14,6 +31,18 @@ struct data{
   int angle; //US
   int anglepolarity; //0 = min, 1 = plus 
   char sensor_type[30];
+};
+
+//AxisSensor
+struct axis{
+  float xaxis;
+  float yaxis;
+  float zaxis;
+  char sensor_type[30];
+};
+
+struct axis AXIS_sensor[] ={
+{0, 0, 0, "9 Axis Sensor"}  
 };
 
 struct data IR_sensor[] ={
@@ -50,10 +79,30 @@ void Sonarstate(){ //Reading Sonar state/distance
   Sonar_sensor[i].distance = Sonar_sensor[i].duration * 0.034 / 2; //calculate the distance in cm
   }}
 
+void AXISstate(){
+ imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+AXIS_sensor[0].xaxis = euler.x();
+AXIS_sensor[0].yaxis = euler.y();
+AXIS_sensor[0].zaxis = euler.z();
+  /* Display the floating point data */
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+
 void setup() {
   //Open Serial Monitor
   Serial.begin(115200); 
   Serial.println("Serial Working"); 
+  
+  //wait for serial port to open!
+  while (!Serial) delay(10); 
+  //Initialise the sensor
+  if(!bno.begin())
+  {
+    //There was a problem detecting the BNO055 ... check your connections
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
   //Declaring IRpins
   for (int i = 0; i<IR_sensoramount; i++){
     pinMode(IR_pins[i], INPUT);
@@ -61,7 +110,7 @@ void setup() {
   for (int i = 0; i<Sonar_sensoramount; i++){
     pinMode(sonarecho_pins[i], INPUT);
     pinMode(sonartrig_pins[i], OUTPUT);
-  }}
+  }delay(1000);}
 
 
 void debug(){ //print the values from the sensors to the Serial Monitor
@@ -87,12 +136,19 @@ void debug(){ //print the values from the sensors to the Serial Monitor
     Serial.print("] : ");
     Serial.print(IR_sensor[i].state);
   }
+  Serial.print(" |     AXIS SENSOR: X: ");
+  Serial.print(AXIS_sensor[0].xaxis);
+  Serial.print(" Y: ");
+  Serial.print(AXIS_sensor[0].yaxis);
+  Serial.print(" Z: ");
+  Serial.print(AXIS_sensor[0].zaxis);
   Serial.println(" |"); 
    }
 
 
 void loop() {
   IRstate();
+  AXISstate();
   Sonarstate();  
   // debug(); // this function is used if you want to see the data in the Serial Monitor
 }
