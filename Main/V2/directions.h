1fd24ab1b3922f15ad85543d728dcfa9f93e14d8
Laurens van der Drift <Lauwy222@hotmail.com>
2023-02-28 10:44:32 +0100

    Added functions to call
    Direction functions added

--- Main/V2/directions.h
+++ Main/V2/directions.h
@@ -15,7 +15,7 @@ const int DIR_Right_Rotation = 116;
 const int DIR_Left_Front_Rotation = 10;
 const int DIR_Right_Front_Rotation = 20;
 const int DIR_Left_Back_Rotation = 96;
-const int DIR_Left_Back_Rotation = 129;
+const int DIR_Right_Back_Rotation = 129;
 const int DIR_drift = 116;
 
 //MotorSpeed
@@ -36,85 +36,52 @@ void MOTOR(int Direction, int MotorPWM1, int MotorPWM2, int MotorPWM3, int Motor
 }
 
 void Forward(){
-MOTOR(DIR_Forward, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-}
+MOTOR(DIR_Forward, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
 void STOP(){
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-}
-void DIR_Backward
-MOTOR(DIR_Backward, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Left_Sideways, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Backward(){
+MOTOR(DIR_Backward, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Right_Sideways, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Left_Sideways(){
+MOTOR(DIR_Left_Sideways, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Left_AAB, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Right_Sideways(){
+MOTOR(DIR_Right_Sideways, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Right_AAB, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Left_AAB(){
+MOTOR(DIR_Left_AAB, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Forward_Left_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Right_AAB(){
+MOTOR(DIR_Right_AAB, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Backward_Left_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Forward_Left_Diagonally(){
+MOTOR(DIR_Forward_Left_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Forward_Right_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Backward_Left_Diagonally(){
+MOTOR(DIR_Backward_Left_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Backward_Right_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Forward_Right_Diagonally(){
+MOTOR(DIR_Forward_Right_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Left_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Backward_Right_Diagonally(){
+MOTOR(DIR_Backward_Right_Diagonally, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Right_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Left_Rotation(){
+MOTOR(DIR_Left_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Left_Front_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Right_Rotation(){
+MOTOR(DIR_Right_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Right_Front_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Left_Front_Rotation(){
+MOTOR(DIR_Left_Front_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Left_Back_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
+void Right_Front_Rotation(){
+MOTOR(DIR_Right_Front_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
 
-MOTOR(DIR_Left_Back_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(2500);
-MOTOR(DIR_STOP, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);
-delay(1000);
-}
+void Left_Back_Rotation(){
+MOTOR(DIR_Left_Back_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
+
+void Right_Back_Rotation(){
+MOTOR(DIR_Left_Back_Rotation, MotorPWM1, MotorPWM2, MotorPWM3, MotorPWM4);}
